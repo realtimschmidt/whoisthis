da31f59b85ac4e1427013fe06c24b5b6971df1cb
realtimschmidt <schmidt.tim.j@gmail.com>
2017-08-30 13:31:07 -0400

    default role added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  before_save { self.role ||= :standard }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  enum role: [:standard, :admin] 
 end
