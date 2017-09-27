a38f4c313b2210b4910d171122c23e3ecaf7294b
realtimschmidt <schmidt.tim.j@gmail.com>
2017-09-27 16:47:26 -0400

    updating tests

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Card do
+  let(:photo) {'image_sample.jpg'}
   let(:name) {Faker::Name.title}
   let(:role) {Faker::Job.title}
   let(:company) {Faker::Company.name}
@@ -8,17 +9,16 @@ RSpec.describe Card do
   let(:school) {Faker::Educator.university}
   let(:title) {Faker::Name.title}
   let(:quiz) { Quiz.create!(title: title) }
-  let(:card) { quiz.cards.create!(name: name, role: role, company: company, city: city, school: school)}
+  let(:card) { quiz.cards.create!(photo: photo, name: name, role: role, company: company, city: city, school: school)}
 
   it { is_expected.to belong_to(:quiz) }
 
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:role) }
-  it { is_expected.to validate_presence_of(:company) }
+  it { is_expected.to validate_presence_of(:photo) }
 
   describe "attributes" do
     it "has photo, name, role, city, and school attributes" do
-      expect(card).to have_attributes(name: name, role: role, company: company, city: city, school: school)
+      expect(card).to have_attributes(photo: photo, name: name, role: role, company: company, city: city, school: school)
     end
   end
 end
--- spec/views/quiz/show.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "quiz/show.html.erb", type: :view do
-#  pending "add some examples to (or delete) #{__FILE__}"
-end
